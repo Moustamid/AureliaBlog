8c22cd51767d221a9d5d2eb149fdb6db00512e2e
Karim Moustamid <Karim1992moustamid@gmail.com>
2022-01-25 14:52:24 -0600

    configure Router

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+import {PLATFORM} from "aurelia-pal";
+
 export class App {
-  message = 'Hello Karim !';
+  
+  constructor()
+  {
+    this. message = 'Hello Karim !';  
+  }
+  
+  configureRouter(config, router)
+  {
+    config.title = 'Karim\'s Blog';
+    config.map([
+      {route : '' , name : 'home' ,  moduleId: PLATFORM.moduleName('posts/index') , title : 'All Posts'}
+    ]);
+  }
+  
 }
---
+++ src/posts/index.js
@@ -0,0 +1,5 @@
+export class Index {
+  constructor() {
+    this.message = 'My Posts Components Sample blog post';
+  }
+}
